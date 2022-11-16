d48d375bed11a38a56f8e6a066c8ddafdf448611
Iñaki Moreno <morenoartabe@gmail.com>
2022-11-16 17:24:08 +0000

    Asset upload done

---
+++ php/assetUpload.php
@@ -0,0 +1,36 @@
+<?php
+echo $_POST['name'];
+echo $_POST['description'];
+echo $_POST['category'];
+echo $_POST['supplier'];
+echo file_get_contents($_FILES['asset']['tmp_name']);
+
+$name = $_POST['name'];
+$description = $_POST['description'];
+$category = $_POST['category'];
+$supplier = $_POST['supplier'];
+$asset = file_get_contents($_FILES['asset']['tmp_name']);
+
+$conn = new mysqli('localhost','root','','beyondrealitydb');
+if($conn->connect_error){
+    
+#connecting error handlers
+    echo "$conn->connect_error";
+    die("Connection Failed : ". $conn->connect_error);
+} else {
+
+    #defining the data that needs to be sent to the database and their data types
+    $stmt = $conn->prepare("insert into assets (name, description, category, supplier, asset) values(?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssss", $name, $description, $category, $supplier, $asset);
+    $execval = $stmt->execute();
+    echo $execval;
+
+    #where the user will be sent once the data is correctly filled in and sent to the database
+    //header("Location: ../html/immersivehub.html");
+    //exit();
+
+    #connection to the datbase closed
+    $stmt->close();
+    $conn->close();
+}
+?>
