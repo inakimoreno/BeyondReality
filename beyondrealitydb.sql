cf672cf3990a06a482341aca43d6a865995a4cb5
Morgansc333 <69326532+Morgansc333@users.noreply.github.com>
2022-11-25 14:17:25 +0000

    25/11

--- beyondrealitydb.sql
+++ beyondrealitydb.sql
@@ -40,7 +40,8 @@ CREATE TABLE `assets` (
 
 CREATE TABLE `users` (
   `userID` int(11) NOT NULL,
-  `username` varchar(20) NOT NULL,
+  `firstname` varchar(20) NOT NULL,
+  `lastname` varchar(20) NOT NULL,
   `email` varchar(30) NOT NULL,
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
